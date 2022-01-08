54c5d40324934f713d882706e383acc7c192c75d
Dustin Kröger <me@staubrein.com>
2022-01-08 06:49:15 +0100

    clear kub

--- drivers/kubernetes.py
+++ drivers/kubernetes.py
@@ -1,4 +1,26 @@
-from drivers import InformalDriverInterface
+from .driver import Driver
+from .exceptions import InvalidDriver, NotRunning, ImageNotFound
+import kubernetes
 
-class KubernetesDriver(InformalDriverInterface):
-    pass
+class KubernetesDriver:
+
+    def __init__(self, raise_errors=True):
+
+        self.raise_errors = raise_errors
+        self._container = None
+        kubernetes.kubernetes.config.load_kube_config()
+
+    def create(self,a,b):
+        pass
+
+    def logs(self):
+        pass
+
+    def stats(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def cleanup(self):
+        pass
